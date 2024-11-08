7613b31b17d132236a02924a77db0415a7213959
femmyte <sanyaoluadefemi@gmail.com>
2024-11-08 15:14:28 +0100

    created internet gateway and route table to be attached to VPC

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -7,6 +7,7 @@ resource "aws_vpc" "agency_vpc" {
   }
 }
 
+# create subnets for RDS instances
 resource "aws_subnet" "rds_subnets" {
   count             = 3
   vpc_id            = aws_vpc.agency_vpc.id
@@ -15,4 +16,43 @@ resource "aws_subnet" "rds_subnets" {
   tags = {
     Name = "${var.RESOURCE_PREFIX}-private-RDS-${element(["a", "b", "c"], count.index)}"
   }
+}
+
+# create an internet gateway
+resource "aws_internet_gateway" "femmyte_igw" {
+  vpc_id = aws_vpc.agency_vpc.id
+
+  tags = {
+    Name = "Agency VPC Internet Gateway"
+  }
+}
+
+# attach the internet gateway to the VPC
+resource "aws_internet_gateway_attachment" "example" {
+  internet_gateway_id = aws_internet_gateway.femmyte_igw.id
+  vpc_id              = aws_vpc.agency_vpc.id
+}
+
+# create a network interface for the RDS instance to use as a network interface for the VPC
+resource "aws_network_interface" "test" {
+  count     = 3
+  subnet_id = aws_subnet.rds_subnets[count.index].id
+}
+
+# create a route table for the VPC
+resource "aws_route_table" "femmyte_rt" {
+  vpc_id = aws_vpc.agency_vpc.id
+  count = 3
+
+  route {
+    cidr_block           = aws_vpc.agency_vpc.cidr_block
+    network_interface_id = aws_network_interface.test[count.index].id
+  }
+}
+
+# associate the route table with the internet gateway
+resource "aws_route_table_association" "b" {
+  gateway_id     = aws_internet_gateway.femmyte_igw.id
+  count = 3
+  route_table_id = aws_route_table.femmyte_rt[count.index].id
 }
